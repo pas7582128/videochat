5e4643117ca11538f050caab0e73030a52aee7ac
Akash <pas7582128@gmail.com>
2020-08-01 21:48:17 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAq4SZDLBcfEcCiPVXvBxhVL05qhv9hr1s",
+    authDomain: "videochat2-pas7582128.firebaseapp.com",
+    databaseURL: "https://videochat2-pas7582128.firebaseio.com",
+    projectId: "videochat2-pas7582128",
+    storageBucket: "videochat2-pas7582128.appspot.com",
+    messagingSenderId: "207023551884",
+    appId: "1:207023551884:web:7b721773ede3a1d924d2a9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
